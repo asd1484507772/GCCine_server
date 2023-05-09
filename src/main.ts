d1894a530355164db64a59a6794f5d8df8a93b8e
wall <1484507772@qq.com>
2023-05-10 00:03:32 +0800

    设置CORS

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,16 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
+
+  // CORS 配置
+  app.enableCors({
+    origin: 'http://localhost:8080',
+    credentials: true, // automatically sets Access-Control-Allow-Credentials header to true
+  });
+
   await app.listen(3333);
 
   if (module.hot) {
